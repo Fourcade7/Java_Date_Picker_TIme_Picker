0f79264f3e3e06e2211e8aacafe0a7fa004cf8c1
Fourcade7 <fourcadeaminov7@gmail.com>
2022-04-24 07:13:04 +0500

    second

--- app/src/main/java/com/fourcade7/java_date_picker_timer_picker/MainActivity.java
+++ app/src/main/java/com/fourcade7/java_date_picker_timer_picker/MainActivity.java
@@ -3,6 +3,8 @@ package com.fourcade7.java_date_picker_timer_picker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +15,7 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1,button2;
+    Button button1,button2,button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +23,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button1=findViewById(R.id.button1);
         button2=findViewById(R.id.button2);
+        button3=findViewById(R.id.button3);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Calendar calendar=Calendar.getInstance();
                 int yaer=calendar.get(Calendar.YEAR);
-                int month=calendar.get(Calendar.DAY_OF_MONTH);
+                int month=calendar.get(Calendar.MONTH);
                 int day=calendar.get(Calendar.DAY_OF_MONTH);
                 DatePickerDialog.OnDateSetListener dateSetListener;
                 dateSetListener=new DatePickerDialog.OnDateSetListener() {
@@ -37,8 +40,30 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, i+"/"+(i1+1)+"/"+i2, Toast.LENGTH_SHORT).show();
                     }
                 };
+                DatePickerDialog datePickerDialog=new DatePickerDialog(MainActivity.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth,dateSetListener,yaer,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
 
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar=Calendar.getInstance();
+                int yaer=calendar.get(Calendar.YEAR);
+                int month=calendar.get(Calendar.MONTH);
+                int day=calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog.OnDateSetListener dateSetListener;
+                dateSetListener=new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+
+                        Toast.makeText(MainActivity.this, i+"/"+(i1+1)+"/"+i2, Toast.LENGTH_SHORT).show();
+                    }
+                };
                 DatePickerDialog datePickerDialog=new DatePickerDialog(MainActivity.this,R.style.ThemeOverlay_Material3_Dialog,dateSetListener,yaer,month,day);
+                //datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 datePickerDialog.show();
             }
         });
